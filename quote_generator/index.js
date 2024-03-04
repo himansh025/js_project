0d8726e2d18b8a6924bc5c5160f7b6ac6fea7faa
himansh025 <himanshudeolia0825@gmail.com>
2024-03-04 19:50:24 +0530

    quote generator for tweeter

---
+++ quote_generator/index.js
@@ -0,0 +1,18 @@
+    
+        let quote= "https://api.quotable.io/random";
+        let author =document.getElementById("author");
+        let add=document.querySelector("blockquote");
+        async function getquote(url){
+            const response = await fetch(url);
+            var data = await response.json();
+            // console.log(data);
+add.innerHTML=data.content;
+author.innerHTML=data.author;
+
+        }
+        getquote(quote);
+
+function postontweeter(){
+    window.open("https://twitter.com/intent/tweet?text=" + add.innerHTML+ "       -----by "+ author.innerHTML,"tweet Window","width=600,height=300")
+
+}
